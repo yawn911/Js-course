51830b160b53f8f8a1b129cf8fa31f211dfce04c
Eugeniy <147269056+shyy911@users.noreply.github.com>
2023-10-17 11:45:09 +0300

    Lesson 1

---
+++ Lesson 1/index.js
@@ -0,0 +1,12 @@
+let a = "Hello World!"
+const b = 8
+var c = 9
+c=1
+
+console.log
+console.log(a + b)
+console.log(b)
+console.log("Sum is: ", c + b + "\n" + "Div is: ", b - c) 
+
+// Today I got acquainted with arithmetic in JS
+// Learn about data types and much more
