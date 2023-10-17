5fcac9f1dd31305d2e2f961db471e3c619cc297e
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-10-17 12:24:02 +0300

    hw2.js

--- Homework 1/hw2.js
+++ Homework 1/hw2.js
@@ -2,7 +2,7 @@ const firstNumber = 22
 const secondNumber = 12 
 const thirdNumber = 24
 
-thirdNumber += 24
+thirdNumber += thirdNumber
 // Sums 
 console.log("Sum of firstNumber and secondNumber:", firstNumber + secondNumber);
 console.log("Sum of secondNumber and thirdNumber: ", secondNumber + thirdNumber);
