4e23dc7995a51245ebe96133a04c8c05cdd70d78
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-10-21 15:52:43 +0300

    hw3.js

--- Homework 2/hw3.js
+++ Homework 2/hw3.js
@@ -1,13 +1,12 @@
-let apples = 5
-let pears = 7
+let apples = 5; // Assign the initial number of apples
+let pears = 7; // Assign the initial number of pears
 
-apples -= 2
-pears -= 2
+apples -= 2; // Subtract 2 apples (Mykola ate 2 apples)
+pears -= 2; // Subtract 2 pears (Mykola ate 2 pears)
 
-if (apples + pears >= 10){
-    console.log(`Hooray, I have ${apples + pears} fruits left`)
-} else{
-    const fruitsNeeded = 10 - (apples + pears)
-    console.log(`I need ${fruitsNeeded} more fruits to make juice`)
+if (apples + pears >= 10) { // Check if there are at least 10 fruits remaining
+    console.log(`Hooray, I have ${apples + pears} fruits left`); // If true, display the number of fruits left
+} else {
+    const fruitsNeeded = 10 - (apples + pears); // Calculate the number of additional fruits needed to make juice
+    console.log(`I need ${fruitsNeeded} more fruits to make juice`); // If false, display the number of fruits needed to make juice
 }
-
