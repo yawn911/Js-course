1b420af7c2c034a9703393e7d53af837d42cb0a7
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-10-21 15:51:53 +0300

    hw2.js

--- Homework 2/hw2.js
+++ Homework 2/hw2.js
@@ -1,8 +1,8 @@
-const currentHour = 10
-const currentMinute = 56
+const currentHour = 10; // Assign the current hour
+const currentMinute = 56; // Assign the current minute
 
-if (currentHour >= 12){
-    console.log(`${currentHour}:${currentMinute}`)
-} else{
-    console.log(`${currentHour}/${currentMinute}`)
+if (currentHour >= 12) { // Check if the current hour is greater than or equal to 12
+    console.log(`${currentHour}:${currentMinute}`); // If true, display the time in 'hh:mm' format
+} else {
+    console.log(`${currentHour}/${currentMinute}`); // If false, display the time in 'hh/mm' format
 }
