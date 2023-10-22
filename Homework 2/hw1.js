e7950c539db77adb06a1bbb69ba31651be90c67e
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-10-22 16:46:22 +0300

    hw1.js

--- Homework 2/hw1.js
+++ Homework 2/hw1.js
@@ -1,16 +1,16 @@
-let a = 2   
-let c = 3
+let a = 2
+let s = 3
 let e = 4
 let x = 5
 
-x += a // added a to x
+a += x
 
-let result1 = a ** e // raised to the power a^e
-let result2 = c ** x // raised to the power c^x
-let mainResult = result1 + result2 // summing 2 powers a^e + c^x
+let answer = a**e + s**x
 
-if (mainResult > 2000) {
-    console.log("The sum of a and x is greater than 2000.") 
-} else {
-    console.log("The sum of a and x is not more than 2000.")
+console.log(`Answer is: ${answer}`)
+
+if (answer > 2000){
+    console.log("number greater than 2000")
+} else{
+    console.log("number less than 2000")
 }
