1c4a38dd003977fc6cff8fca0d2c7f207a44963d
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-10-24 14:50:26 +0300

    hw2.js

--- Homework 3/hw2.js
+++ Homework 3/hw2.js
@@ -1,7 +1,7 @@
 const number = 16
 
 if (number % 2 === 0 && number % 4 === 0) {
-    console.log("The number is even and divisible by 4")
+    console.log(`The ${number} is even and divisible by 4`)
 } else {
-    console.log("The number does not satisfy the condition")
+    console.log(`The ${number} does not satisfy the condition`)
 }
