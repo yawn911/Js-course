540ed1ff40c4fd7bca2f3f30d67b07cd59e8ab62
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-10-26 11:57:42 +0300

    hw3.js

--- Homework 3/hw3.js
+++ Homework 3/hw3.js
@@ -1,7 +1,7 @@
-const number = 15;
+const number = 15
 
-if (number % 3 === 0 || number % 5 === 0) {
-    console.log("The number is divisible by 3 or 5 without a remainder")
-} else {
-    console.log("The number is not divisible by 3 or 5 without a remainder")
-} 
+if(number % 3 === 0 || 5 === 0){
+    console.log(`The ${number} is divisible by 3 or 5 without a remainder`)
+}else{
+    console.log(`The ${number} is not divisible by 3 or 5 without a remainder`)
+}
