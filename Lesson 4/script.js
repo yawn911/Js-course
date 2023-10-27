c5c18db51fb2d6061322f6a5a81a6e816f52a6d4
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-10-27 19:06:40 +0300

    Lesson 4

---
+++ Lesson 4/script.js
@@ -0,0 +1,55 @@
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+console.log("Hello")
+
+let num = 4
+
+
+while(num >= 1){
+    console.log(num)
+    num--
+}
+
+
+let num = 1
+
+while(num <= 11){
+    console.log(num-1)
+    num++
+}
+
+
+for(let i = -10; i < 11; i++){
+    console.log(i)
+}
+
+for(let a = 10; a > -1; a--){
+    console.log(a)
+}
+
+let e = 0
+do {
+    console.log(e)
+    e++
+} while (e < 3);
+
+let a = 21
+
+while(a > 0){
+    if(a % 5 === 0 ){
+    console.log(a)
+    
+}
+a--
+}
+
+
+
+
+
