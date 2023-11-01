465fbb576768c961da25f7328be610277bac15d2
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-11-01 19:07:11 +0300

    Update hw3.js

--- Homework 5/hw3.js
+++ Homework 5/hw3.js
@@ -1,11 +1,9 @@
-var y = 5; 
+var y = 5;
 
-if (y > 1) {
-    var predecessor = y - 1;
-    console.log("The neighbor to the left of", y, "is", predecessor); // The neighbor to the left of 5 is 4
-} else {
-    console.log("The number", y, "has no neighbor to the left.");
+
+for (var i = 1; i <= y; i++) {
+    var leftNeighbor = i - 1;
+    var rightNeighbor = i + 1;
+
+    console.log("Number", i, "Immediate neighbors:", leftNeighbor, rightNeighbor);
 }
-
-var successor = y + 1;
-console.log("The neighbor to the right of", y, "is", successor); // The neighbor to the right of 5 is 6
