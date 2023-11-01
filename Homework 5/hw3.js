033a7d0591f4cb34548b3243f5d406f0579b7e57
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-11-01 19:09:59 +0300

    Update hw3.js

--- Homework 5/hw3.js
+++ Homework 5/hw3.js
@@ -5,5 +5,5 @@ for (var i = 1; i <= y; i++) {
     var leftNeighbor = i - 1;
     var rightNeighbor = i + 1;
 
-    console.log("Number", i, "Immediate neighbors:", leftNeighbor, rightNeighbor);
+    console.log(`Number ${i} Immediate neighbors: ${leftNeighbor} ${rightNeighbor}`);
 }
