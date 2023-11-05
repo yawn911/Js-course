2da15e25013e395508e813b522bad2ade2271242
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-11-05 22:05:52 +0300

    Update hw1.js

--- Homework 6/hw1.js
+++ Homework 6/hw1.js
@@ -1,27 +1,27 @@
 function sum(first, second) {
-    return(5 + 8)
+    return first + second;
 }
 
-let result1 = sum(5, 8)
-console.log(result1)
+let result1 = sum(5, 8);
+console.log(result1);
 
 function sub(first, second) {
-    return(5 - 4)
+    return first - second;
 }
 
-let result2 = 5 - 4
-console.log(result2)
+let result2 = sub(5, 4);
+console.log(result2);
 
 function div(first, second) {
-    return(20 / 5)
+    return first / second;
 }
 
-let result3 = div(20, 5)
-console.log(result3)
+let result3 = div(20, 5);
+console.log(result3);
 
 function multi(first, second) {
-    return(20 * 10)
+    return first * second;
 }
 
-let result4 = 20 * 10
-console.log(result4)
+let result4 = multi(20, 10);
+console.log(result4);
