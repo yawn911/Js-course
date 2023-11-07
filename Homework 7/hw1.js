8c35786be7e93432c93ef418c544171fcb7a36ab
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-11-08 00:24:15 +0300

    Homework 7

---
+++ Homework 7/hw1.js
@@ -0,0 +1,6 @@
+let sum = (a) =>{
+    let result = a**2;
+    return result;
+}
+
+console.log(sum(6))
---
+++ Homework 7/hw2.js
@@ -0,0 +1,9 @@
+let fruits = ['apple', 'banana', 'cherry', 'grape'];
+
+function getFruits(fruits){
+    for(let i = fruits.length - 1; i >= 0; i--){
+    console.log(fruits[i])
+    }
+}
+
+getFruits(fruits);
