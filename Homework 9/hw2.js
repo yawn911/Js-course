e68e10667104e271cf6e5b15e0b5aef7df20b635
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-11-19 22:36:47 +0300

    Update hw2.js

--- Homework 9/hw2.js
+++ Homework 9/hw2.js
@@ -1,13 +1,15 @@
 let randomNumbers = [4, 10, 14, 96, 128, 3];
+let randomNumbers2 = [5, 13, 67, 84, 200];
 
-function getHighestNumber() {
-  let highestNumber = randomNumbers[0];
-  for (let i = 0; i < randomNumbers.length; i++) {
-    if (randomNumbers[i] > highestNumber) {
-      highestNumber = randomNumbers[i];
+function getHighestNumber(array) {
+  let highestNumber = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > highestNumber) {
+      highestNumber = array[i];
     }
   }
   return highestNumber;
 }
 
-console.log(getHighestNumber());
+console.log(getHighestNumber(randomNumbers));
+console.log(getHighestNumber(randomNumbers2));
