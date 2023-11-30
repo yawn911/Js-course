7b3c707d7e03e94e0ea7f047868ac930b80b75c6
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-11-30 18:21:22 +0100

    Lesson 13

---
+++ Lesson 13/index.js
@@ -0,0 +1,6 @@
+// // index.js
+const { getNumber, getFullName } = require("./script");
+
+console.log(getFullName("John", "Doe"));
+console.log(getNumber(10));
+
---
+++ Lesson 13/script.js
@@ -0,0 +1,10 @@
+// script.js
+const getFullName = (firstname, lastName) => {
+  return `My fullname is ${firstname} ${lastName}`;
+};
+
+function getNumber(num) {
+  return num + 5;
+}
+
+module.exports = { getNumber, getFullName };
