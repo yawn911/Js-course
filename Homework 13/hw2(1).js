6ac017288857f9e7d9e09aa3e47bde47b3ba73b4
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-12-01 20:20:33 +0100

    Add files via upload

---
+++ Homework 13/hw2(1).js
@@ -0,0 +1,3 @@
+const name = "John"
+
+module.exports = name;
---
+++ Homework 13/hw2Import.js
@@ -0,0 +1,9 @@
+const hello = require("./hw2");
+const name = require("./hw2(1)");
+
+function printGreetings() {
+  let greetings = `${hello} ${name}!`;
+  console.log(greetings);
+}
+
+printGreetings();
