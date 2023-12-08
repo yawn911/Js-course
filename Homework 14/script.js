e9d0bd0065cfd43fcb2adebf48a238ae6b7f5ddf
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-12-08 19:07:40 +0100

    Homework 14

---
+++ Homework 14/script.js
@@ -0,0 +1,17 @@
+let kids = {
+    name: "Jhon",
+    age: 18,
+    sex: "male",
+  },
+  kids2 = {
+    name: "Diana",
+    age: 14,
+    sex: "female",
+  },
+  kids3 = {
+    name: "Lisa",
+    age: 16,
+    sex: "female",
+  };
+
+  module.exports = {kids, kids2, kids3}
---
+++ Homework 14/scriptImport.js
@@ -0,0 +1,5 @@
+const { kids, kids2, kids3 } = require("./script");
+
+console.log(kids.name, kids.sex, kids.age);
+console.log(kids2.name, kids2.sex, kids2.age);
+console.log(kids3.name, kids3.sex, kids3.age);
