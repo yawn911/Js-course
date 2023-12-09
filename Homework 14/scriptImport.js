84753061fcda85de17b35bde19b116c2a5d872f8
Eugeniy <147269056+yawn911@users.noreply.github.com>
2023-12-09 16:30:52 +0300

    Update scriptImport.js

--- Homework 14/scriptImport.js
+++ Homework 14/scriptImport.js
@@ -1,13 +1,7 @@
 const { person } = require("./script");
 
-const isFirstKidOlder = person.kids[0].age > person.kids[1].age;
+person.kids.sort((a, b) => a.age - b.age);
 
-if (isFirstKidOlder) {
-  console.log(person.kids[0].name.toUpperCase());
-  console.log(person.kids[1].name.toLowerCase());
-} else {
-  console.log(person.kids[1].name.toUpperCase());
-  console.log(person.kids[0].name.toLowerCase());
+for (let i = 0; i < person.kids.length; i++) {
+  console.log(person.kids[i].name);
 }
-
-console.log(person.kids[2].name.toLowerCase());
